a27bba6ffe802f15d27bf16dd9d6ed4920ab3e33
George2368 <zaikovgeorge2018@gmail.com>
2025-08-19 22:46:33 +0300

    Start

--- planto/src/App.js
+++ planto/src/App.js
@@ -2,20 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App-planto">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
